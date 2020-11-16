445b8fe78848a524d1ec8a7692cfdaedfa391968
Sergei Zhukov <soberzhukov@mail.ru>
2020-11-16 04:17:46 +0400

    'Carcass'

---
+++ Homework_1.py
@@ -0,0 +1,65 @@
+
+# Общие параметры (кличка, голос, вес)
+
+class Goose:
+    def __init__(self, name, vote, weight=0, eggs=0):
+        self.name = name
+        self.vote = vote
+        self.weight = weight
+        self.eggs = eggs
+    def feed(self):
+        pass
+
+
+
+class Cow:
+    def __init__(self, name, vote, weight=0, milk=0):
+        self.name = name
+        self.vote = vote
+        self.weight = weight
+        self.milk = milk
+    def feed(self):
+        pass
+
+class Sheep:
+    def __init__(self, name, vote, weight=0, wool=0):
+        self.name = name
+        self.vote = vote
+        self.weight = weight
+        self.wool = wool
+    def feed(self):
+        pass
+
+
+class Chicken:
+    def __init__(self, name, vote, weight=0, eggs=0):
+        self.name = name
+        self.vote = vote
+        self.weight = weight
+        self.eggs = eggs
+    def feed(self):
+        pass
+
+
+
+class Goat:
+    def __init__(self, name, vote, weight=0, milk=0):
+        self.name = name
+        self.vote = vote
+        self.weight = weight
+        self.milk = milk
+    def feed(self):
+        pass
+
+class Duck:
+    def __init__(self, name, vote, weight=0, eggs=0):
+        self.name = name
+        self.vote = vote
+        self.weight = weight
+        self.eggs = eggs
+    def feed(self):
+        pass
+
+
+
+
