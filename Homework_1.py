f1b8205b54382b597013f8c0a535fcbebd970f03
Sergei Zhukov <soberzhukov@mail.ru>
2020-11-22 08:22:45 +0400

    Homework 1. subclass

--- Homework_1.py
+++ Homework_1.py
@@ -1,16 +1,27 @@
-
-# Общие параметры (кличка, голос, вес)
-
-
 class Animal:
     def __init__(self, name, weight=0):
         self.name = name
         self.weight = weight
+
     def feed(self):
         self.weight += 1
 
+    def voise(self):
+        animal_voise = {Cow:'мууу',
+                  Goose: 'гагага',
+                  Sheep: 'беее',
+                  Chicken:'ко-ко-ко',
+                  Goat:'бэээ',
+                  Duck:'кря-кря'
+                  }
+        for k,v in animal_voise.items():
+            if type(self) == k:
+                return v
+
+
 class Bird:
-    def __init__(self, name, weight=0):
+    def __init__(self, name, weight=0, eggs=0):
+        self.eggs = eggs
         self.name = name
         self.weight = weight
 
@@ -20,19 +31,12 @@ class Bird:
         else:
             print('Нечего собирать')
 
-class Goose(Animal, Bird):
-    def __init__(self, name, weight=0, eggs=0):
-        self.vote = 'га-га-га'
-        self.eggs = eggs
-        super().__init__(name, weight)
 
-
-
-class Cow(Animal):
+class Givemilk:
     def __init__(self, name, weight=0, milk=0):
-        super().__init__(name, weight)
-        self.vote = 'муууу'
         self.milk = milk
+        self.name = name
+        self.weight = weight
 
     def collect_milk(self):
         if self.milk != 0:
@@ -41,11 +45,24 @@ class Cow(Animal):
             print('Нечего собирать')
 
 
+class Goose(Animal, Bird):
+    def __init__(self, name, weight=0, eggs=0):
+        self.eggs = eggs
+        super().__init__(name, weight)
+
+
+
+class Cow(Animal, Givemilk):
+    def __init__(self, name, weight=0, milk=0):
+        self.milk = milk
+        super().__init__(name, weight)
+
+
+
 class Sheep(Animal):
     def __init__(self, name, weight=0, wool=0):
-        super().__init__(name, weight)
-        self.vote = 'беее'
         self.wool = wool
+        super().__init__(name, weight)
 
     def collect_wool(self):
         if self.wool != 0:
@@ -57,39 +74,25 @@ class Sheep(Animal):
 
 class Chicken(Animal, Bird):
     def __init__(self, name, weight=0, eggs=0):
-        super().__init__(name, weight)
-        self.vote = 'ко-ко-ко'
         self.eggs = eggs
-
-
-
-
-
-class Goat(Animal):
-    def __init__(self, name, weight=0, milk=0):
         super().__init__(name, weight)
-        self.vote = 'мееее'
+
+
+
+class Goat(Animal, Givemilk):
+    def __init__(self, name, weight=0, milk=0):
         self.milk = milk
-
-    def collect_milk(self):
-        if self.milk != 0:
-            self.milk -= 1
-        else:
-            print('Нечего собирать')
-
-
+        super().__init__(name, weight)
 
 
 class Duck(Animal, Bird):
     def __init__(self, name, weight=0, eggs=0):
-        super().__init__(name, weight)
-        self.vote = 'кря кря'
         self.eggs = eggs
+        super().__init__(name, weight)
 
 
 
-
-gray_goose = Goose('Серый', 4, 6)
+gray_goose = Goose('Серый', 4, 5)
 white_goose = Goose('Белый', 3, 4)
 mania_cow = Cow('Манька', 200, 15)
 barashek_sheep = Sheep('Барашек', 50, 4)
@@ -122,7 +125,25 @@ def weight_total_and_heaviest():
             favorite_animal = animal.name
     return f'Общий вес: {weight_sum}. Самое тяжелое животное: {favorite_animal}'
 
-#print(weight_total_and_heaviest())
+print(weight_total_and_heaviest())
 
-
-
+def print_all_metods_animals():
+    list_animals = [
+        gray_goose,
+        white_goose,
+        mania_cow,
+        barashek_sheep,
+        kudri_sheep,
+        ko_ko_cheecken,
+        kuka_cheecken,
+        roga_goat,
+        kopita_goat,
+        kria_duck,
+    ]
+    for animal in list_animals:
+        print(f'{animal.name} издает звук {animal.voise()}')
+        print(f'Его вес до кормешки:{animal.weight}')
+        animal.feed()
+        print(f'Его вес после кормешки:{animal.weight}')
+        print()
+print_all_metods_animals()
